7016317b51862a632ad396ee4332dd2dd5dc72b4
fahreeve <fahreeve@yandex.ru>
2015-04-13 13:50:28 +0300

    add tests for HEAD method

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer
-import urllib.request
 import unittest
+import http.client
+import multiprocessing
 import webdav
 import os
 
@@ -36,22 +37,45 @@ class GetAbsoluteURL(unittest.TestCase):
         fullpath = webdav.get_absolute_path(path)
         self.assertEqual(fullpath, os.path.join(os.getcwd(), webdav.FILE_DIR, "dir", "dir/"))
 
-'''class DELETE_method(unittest.TestCase):
+class HEAD_method(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(HEAD_method, cls).setUpClass()
+        cls.port = 8080
+        cls.url = "localhost"
+        httpd = HTTPServer(("", cls.port), webdav.WebDavHandler)
+        cls.server = multiprocessing.Process(target=httpd.serve_forever)
+        cls.server.start()
 
-    def setUp(self):
-        self.port = 8080
-        self.url = "127.0.0.1"
-        self.server = HTTPServer(("", self.port), webdav.WebDavHandler)
+    def test_head1(self):
+        conn = http.client.HTTPConnection(self.url, self.port)
+        conn.request("HEAD", "/")
+        res = conn.getresponse()
+        self.assertEqual(res.status, 200)
 
-    def test_delete_file(self):
-        req = urllib.request.Request('http://{}:{}/'.format(self.url, self.port), method='OPTIONS')
-        resp = req.read()#urllib.request.urlopen(req)
-        print(resp.status)
-        pass
+    def test_head2(self):
+        conn = http.client.HTTPConnection(self.url, self.port)
+        conn.request("HEAD", "")
+        res = conn.getresponse()
+        self.assertEqual(res.status, 200)
 
-    def tearDown(self):
-        self.server.socket.close()
-'''
+    def test_head3(self):
+        conn = http.client.HTTPConnection(self.url, self.port)
+        conn.request("HEAD", "/file")
+        res = conn.getresponse()
+        self.assertEqual(res.status, 200)
+
+    def test_head4(self):
+        conn = http.client.HTTPConnection(self.url, self.port)
+        conn.request("HEAD", "/dir/")
+        res = conn.getresponse()
+        self.assertEqual(res.status, 200)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.terminate()
+        super(HEAD_method, cls).setUpClass()
+
 
 if __name__ == '__main__':
     unittest.main()
